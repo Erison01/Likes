c1a55d2930b647139619d322dae9d09f1b0e3d28
Erison <erisonn01@protonmail.ch>
2023-05-08 16:50:37 -0700

    commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function counterAdd(){
+counter.innerText=Number(counter.innerText)+1
+}
+function countAdd(){
+    count.innerText=Number(count.innerText)+1
+}
+function counterrAdd(){
+    counterr.innerText=parseInt(counterr.innerText)+1
+}
